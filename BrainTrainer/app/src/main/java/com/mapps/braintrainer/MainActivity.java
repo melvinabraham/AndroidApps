35693100de98243376fd85dc1922c3f4426f7505
melvinabraham <melvin.abraham1996@gmail.com>
2017-01-20 02:34:54 +0530

    Added Layout for BrainTrainer App

---
+++ BrainTrainer/app/src/main/java/com/mapps/braintrainer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mapps.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button startButton;
+    public void Start(View view)    {
+
+        startButton.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
+
+
+    }
+}
